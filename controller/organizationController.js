43e8adefd1b1b3b8b90dbbda0d22aa2cf6977cee
Bhairab-Nath <nathbhairab22@gmail.com>
2025-08-30 00:01:39 +0545

    userhistory delete organizations

--- controller/organizationController.js
+++ controller/organizationController.js
@@ -6,7 +6,7 @@ exports.createOrganization = async(req,res)=>{
     const {name,address,email,number} = req.body
     const userId = req.userId
     const OrganizationNumber = Math.floor(1000 + Math.random()*9000)
-    await sequelize.query(`CREATE TABLE organization_${OrganizationNumber}(
+    await sequelize.query(`CREATE TABLE IF NOT EXISTS organization_${OrganizationNumber}(
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255),
         address VARCHAR(255),
@@ -18,14 +18,55 @@ exports.createOrganization = async(req,res)=>{
             type : QueryTypes.CREATE
     })
 
+    await sequelize.query(`CREATE TABLE IF NOT EXISTS userHistory_${userId}(
+        organizationNumber INT
+        
+        )`,{
+            type: QueryTypes.CREATE
+    })
+
     await sequelize.query(`INSERT INTO organization_${OrganizationNumber}(name,address,email,number,userId) VALUES(?,?,?,?,?)`,{
         type : QueryTypes.INSERT,
         replacements : [name,address,email,number,userId]
+    })
+
+    await sequelize.query(`INSERT INTO userHistory_${userId}(organizationNumber) VALUES(?)`,{
+        type: QueryTypes.INSERT,
+        replacements: [OrganizationNumber]
     })
 
     res.status(200).json({
             message: "Organization Created successfully.",
             orgNo: OrganizationNumber
     })
+
+}
+
+exports.deleteUser = async(req,res)=>{
+const userId = req.userId
+
+const userOrganizations = await sequelize.query(`SELECT organizationNumber FROM userHistory_${userId}`,{
+    type: QueryTypes.SELECT
+})
+
+await sequelize.query(`DELETE FROM users WHERE id= ?`,{
+    type: QueryTypes.DELETE,
+    replacements: [userId]
+})
+
+for(var i = 0; i<userOrganizations.length; i++){
+    await sequelize.query(`DROP TABLE organization_${userOrganizations[i].organizationNumber}`,{
+        type: QueryTypes.RAW
+    })
+}
+
+await sequelize.query(`DROP TABLE userHistory_${userId}`,{
+    type: QueryTypes.RAW
+})
+
+res.status(200).json({
+    message: "All organization deleted"
+})
+
 
 }
--- routes/organizationRoute.js
+++ routes/organizationRoute.js
@@ -1,8 +1,9 @@
-const { createOrganization } = require('../controller/organizationController')
+const { createOrganization, deleteUser } = require('../controller/organizationController')
 const { isAuthenticated } = require('../middleware/isAuthenticated')
 
 const router = require('express').Router()
 
 router.route('/organization').post(isAuthenticated, createOrganization)
+router.route('/deleteuser').delete(isAuthenticated, deleteUser)
 
 module.exports = router
